06a47b0d178f87c415d322c3d466a75ccd70931e
Lucasftn8 <47553598+Lucasftn8@users.noreply.github.com>
2019-12-16 19:38:00 +0100

    Add files via upload

---
+++ RaspTempManager/src/ee402test/DataPackage.java
@@ -0,0 +1,30 @@
+package ee402test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DataPackage {
+	   // Client DataPackage
+        private String t_temp,t_time;// value of reference
+		
+		public DataPackage() {}
+		public DataPackage getDataPackage() {
+			//ouvrir le fichier et donner le t_temp à l'adresse /sys/class/thermal/thermal_zone0/temp
+			try {
+				File file = new File("/sys/class/thermal/thermal_zone0/temp");
+				Scanner scan = new Scanner(file);
+				t_temp= scan.nextLine();
+				System.out.println(t_temp);
+				scan.close();
+			}
+			catch (FileNotFoundException e){
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+			DateTimeService datatime=new DateTimeService();
+			t_time= datatime.getDateAndTime();
+			return this;
+		}
+		public String get_data_str() {return (t_temp+"@"+t_time);}
+}
